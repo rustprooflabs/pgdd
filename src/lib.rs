91e02bc9ed210b254d11596cc6b4eef326c831f7
Ryan Lambert <ryan@rustprooflabs.com>
2021-04-10 07:03:37 -0600

    Configure tables to pg_dump custom data #11

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,17 @@ COMMENT ON COLUMN dd.meta_column.sensitive
 INSERT INTO dd.meta_column (s_name, t_name, c_name, data_source, sensitive)
     VALUES ('dd', 'meta_column', 'sensitive', 'Manually defined', False)
 ;
+
+
+SELECT pg_catalog.pg_extension_config_dump('dd.meta_schema'::regclass, 'WHERE s_name <> ''dd'' ');
+SELECT pg_catalog.pg_extension_config_dump('dd.meta_schema_meta_schema_id_seq'::regclass, '');
+
+SELECT pg_catalog.pg_extension_config_dump('dd.meta_table'::regclass, 'WHERE s_name <> ''dd'' ');
+SELECT pg_catalog.pg_extension_config_dump('dd.meta_table_meta_table_id_seq'::regclass, '');
+
+SELECT pg_catalog.pg_extension_config_dump('dd.meta_column'::regclass, 'WHERE s_name <> ''dd'' ');
+SELECT pg_catalog.pg_extension_config_dump('dd.meta_column_meta_column_id_seq'::regclass, '');
+
 "#
 );
 
