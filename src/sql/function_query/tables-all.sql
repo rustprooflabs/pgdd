e482d79402a50ee46e47b8064cae81c7ea5a43a7
Ryan Lambert <ryan@rustprooflabs.com>
2022-01-23 07:59:51 -0700

    Add size_plus_indexes_bytes column to schemas, tables, and views

--- src/sql/function_query/tables-all.sql
+++ src/sql/function_query/tables-all.sql
@@ -16,7 +16,9 @@ SELECT n.nspname::TEXT AS s_name,
                 THEN (pg_table_size(c.oid::regclass)::double precision / c.reltuples)::BIGINT
             ELSE NULL::BIGINT
         END AS bytes_per_row,
+    pg_total_relation_size(c.oid::regclass)::BIGINT AS size_plus_indexes_bytes,
     pg_size_pretty(pg_total_relation_size(c.oid::regclass))::TEXT AS size_plus_indexes,
+
     obj_description(c.oid, 'pg_class'::name)::TEXT AS description,
         CASE
             WHEN n.nspname !~ '^pg_toast'::text AND (n.nspname <> ALL (ARRAY['pg_catalog'::name, 'information_schema'::name])) THEN false
